315b5098742e4f365cbe9866f4b2480bc4f7346a
nakaoyuka <58930395+nakaoyuka@users.noreply.github.com>
2020-06-01 10:39:00 +0900

    一覧変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_095657) do
+ActiveRecord::Schema.define(version: 2020_05_31_072355) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "date"
+    t.datetime "date", null: false
     t.integer "category"
     t.string "title"
     t.string "text"
-    t.integer "finished"
+    t.datetime "finished", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
